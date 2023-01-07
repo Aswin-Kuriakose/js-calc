544a5a8d440de67ce0cf52e01b012521afffc903
Aswin <aswin.kuriakose@vonnue.com>
2023-01-07 17:09:55 +0530

    js started

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputEl = document.getElementById("input-el")
+const numBttn = document.querySelectorAll(".buttons")
+const operatorBttn = document.querySelectorAll(".operants")
+const clearBttn = document.getElementById("clear-bttn")
+const equalBttn = document.getElementById("equal-bttn")
+
+
+
+
